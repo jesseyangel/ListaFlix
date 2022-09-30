2f43d82cc5af5791fd3f2245c5db15ac4a9348cb
Jessey Angel <103680584+jesseyangel@users.noreply.github.com>
2022-09-30 00:22:15 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
 function adicionarFilme(){
-    
+  var campoFilmeFavorito = document.getElementById("filme").value;
+  var elementoFilmeFavorito = "<img src=" + campoFilmeFavorito + ">"
+  var elemntoListaFilmes = document.getElementById("listaFilmes")
+  elementoListaFilmes.innerHTML = elementoFilmeFavorito
 }

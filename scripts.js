e1d95aa9ae6cf350f1b71aee266730783eaf6413
Jessey Angel <103680584+jesseyangel@users.noreply.github.com>
2022-09-30 02:28:06 -0300

    quebrando código

--- scripts.js
+++ scripts.js
@@ -1,11 +1,17 @@
 function adicionarFilme() {
   var filmeFavorito = document.getElementById("filme").value;
   if (filmeFavorito.endsWith(".jpg")) {
-  var elementoFilmeFavorito = "<img src=" + filmeFavorito + ">"
-  var elementoListaFilmes = document.getElementById("listaFilmes")
-  elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoFilmeFavorito;
+    listarFilmesNaTela(filmeFavorito);
 } else { 
   console.error("Endereço de filme inválido");
 }
 document.getElementById("filme").value = "";
+}
+
+function listarFilmesNaTela(filme) {
+  console.log(filme);
+  var elementoFilmeFavorito = "<img src=" + filme + ">";
+  var elementoListaFilmes = document.getElementById("listaFilmes");
+  elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoFilmeFavorito;
+
 }
